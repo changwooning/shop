8f997f0340d2298d344a8b6f6b14a5a96bb5221f
sonchangwoo <thsckddn97@naver.com>
2024-03-21 18:18:28 +0900

    Add : createUser Method

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -1,6 +1,31 @@
 package shop;
-
+import java.util.ArrayList;
+import java.util.Random;
 public class UserManager {
-
-
+	
+	private Random ran;
+	
+	private ArrayList<User> group;
+	
+	private UserManager() {
+		this.group = new ArrayList<>();
+		this.ran = new Random();
+	}
+	// 클래스 내부에서 단일객체 생성하기 
+	private static UserManager userManager = new UserManager();
+	// 클래스 내부에서 게터 제공
+	public static UserManager getUserManager() {
+		return userManager;
+	}
+	
+	
+	// 기능 CRUD
+	
+	// C
+	public void createUser(String name,String id,String password) {
+		User user = new User(id,password,name);
+		group.add(user);
+	}
+	
+	
 }
